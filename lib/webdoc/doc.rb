301a386bd84eb952080e47d3db7d808e2c64e15f
Dimitri Kurashvili <dimitri@c12.ge>
2013-05-26 15:43:59 +0400

    emphasize picture

--- lib/webdoc/doc.rb
+++ lib/webdoc/doc.rb
@@ -25,62 +25,52 @@ module Webdoc
     end
 
     def take_picture(name, h = {})
+      h = h.symbolize_keys
+      border_w = 5
       path = File.join(@save_image, name)
       screen_w = h[:width] || 1000
       screen_h = h[:height] || 500
       save_screenshot(path, width: screen_w, height: screen_h)
-      if h[:crop]
-        selector = h[:crop]
+
+      image = Image.read(path).first
+      crop = h[:crop]
+      emphasize = h[:emphasize]
+
+      if crop
         page.driver.resize_window(screen_w, screen_h)
-        w = evaluate_script("$('#{selector}').width()")
-        h = evaluate_script("$('#{selector}').height()")
-        l = evaluate_script("$('#{selector}').offset().left")
-        t = evaluate_script("$('#{selector}').offset().top")
-        image = Image.read(path).first
-        image.crop!(l, t, w, h)
-        image.write(path)
+        w, h, l, t = dimensions(crop)
+        image = image.crop(l - border_w, t - border_w, w + 2*border_w, h + 2*border_w)
+      else
+        w, h, l, t = [ screen_w, screen_h, 0, 0 ]
       end
-      nil
+
+      if emphasize
+        page.driver.resize_window(screen_w, screen_h)
+        ew, eh, el, et = dimensions(emphasize)
+        element_image = image.crop(el, et, ew, eh)
+        image = image.blur_image(100, 1)
+        image = image.composite(
+          element_image.border(border_w, border_w, 'rgba(255,0,0,0.5)'),
+          el - l,
+          et - t,
+          Magick::OverCompositeOp
+        )
+      end
+
+      image.write(path)
     end
 
     def add_picture(name, h = {})
       path = File.join(@image_url, name)
       self << div({ class: 'image' }, img({ src: path }))
     end
+
+    def dimensions(selector)
+      w = evaluate_script("$('#{selector}').outerWidth()")
+      h = evaluate_script("$('#{selector}').outerHeight()")
+      l = evaluate_script("$('#{selector}').offset().left")
+      t = evaluate_script("$('#{selector}').offset().top")
+      [w, h, l, t]
+    end
   end
 end
-
-# Capybara.run_server = false
-# Capybara.current_driver = :webkit
-# Capybara.app_host = 'http://www.google.com'
-
-# module Webdoc
-#   class SimpleTest
-#     include Capybara::DSL
-#     include Magick
-
-#     def test_google
-#       w = 800
-#       h = 300
-#       page.driver.resize_window(w, h)
-#       visit '/'
-#       el = find('input[name=q]')
-#       el.set('Dimitri Kurashvili')
-#       save_screenshot('/Users/dimitri/Desktop/screenshot.png', width: w, height: h)
-
-#       # img = Image.read('/Users/dimitri/Desktop/screenshot.png').first
-#       # img2 = img.blur_image(100, 1)
-#       # x = 250
-#       # y = 208
-#       # w = 20
-#       # img3 = img.crop(x, y, 468, 35)
-#       # img4 = img2.composite(img3.border(w, w, 'rgba(0,0,0,0.3)'), x-w, y-w, Magick::OverCompositeOp)
-#       # img2.write('/Users/dimitri/Desktop/screenshot2.png')
-#       # img3.write('/Users/dimitri/Desktop/screenshot3.png')
-#       # img4.write('/Users/dimitri/Desktop/screenshot3.png')
-#     end
-#   end
-# end
-
-# t = Webdoc::SimpleTest.new
-# t.test_google
